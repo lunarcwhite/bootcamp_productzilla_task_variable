1d2ad4e31f106107371367177a4d0dced3c8f527
lunarcwhite <rizkym058@gmail.com>
2023-02-28 20:10:14 +0700

    bootcamp productzilla task variable

---
+++ nomber-1.js
@@ -0,0 +1,11 @@
+const angka1 = 10 * 80 + 10
+const angka2 = 100 - 10 + 20
+const angka3 = 80 + 80 / 2
+const angka4 = 100 % 2 + 1
+const angka5 = 70 - 10 % 5
+
+console.log(angka1)
+console.log(angka2)
+console.log(angka3)
+console.log(angka4)
+console.log(angka5)
---
+++ nomber-2.js
@@ -0,0 +1,33 @@
+let lebihBesar1 = 10 > 10
+let lebihBesar2 = 10 > 100
+let lebihkecil1 = 20 < 18
+let lebihkecil2 = 49 < 82
+let lebihBesarSamaDengan1 = 90 >= 90
+let lebihBesarSamaDengan2 = 100 >= 29
+let lebihKecilSamaDengan1 = 27 <= 72
+let lebihKecilSamaDengan2 = 36 <= 71
+let equal1 = 10 == 10
+let equal2 = 90 == 10
+let notEqual1 = 90 != 73
+let notEqual2 = 28 != 37
+let equalTo1 = 10 === '10'
+let equalTo2 = 90 === '90'
+let notEqualTo1 = 72 !== '82'
+let notEqualTo2 = true !== 1
+
+console.log(lebihBesar1)
+console.log(lebihBesar2)
+console.log(lebihkecil1)
+console.log(lebihkecil2)
+console.log(lebihBesarSamaDengan1)
+console.log(lebihBesarSamaDengan2)
+console.log(lebihKecilSamaDengan1)
+console.log(lebihKecilSamaDengan2)
+console.log(equal1)
+console.log(equal2)
+console.log(equalTo1)
+console.log(equalTo2)
+console.log(notEqual1)
+console.log(notEqual2)
+console.log(notEqualTo1)
+console.log(notEqualTo2)
