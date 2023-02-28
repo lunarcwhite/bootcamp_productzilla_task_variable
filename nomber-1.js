29df58e62e4a7b7bbc1fdf5abc16661bdcea78b4
lunarcwhite <rizkym058@gmail.com>
2023-02-28 20:30:48 +0700

    merapihkan posisi

--- nomber-1.js
+++ nomber-1.js
@@ -1,11 +1,10 @@
 const angka1 = 10 * 80 + 10
-const angka2 = 100 - 10 + 20
-const angka3 = 80 + 80 / 2
-const angka4 = 100 % 2 + 1
-const angka5 = 70 - 10 % 5
-
 console.log(angka1)
+const angka2 = 100 - 10 + 20
 console.log(angka2)
+const angka3 = 80 + 80 / 2
 console.log(angka3)
+const angka4 = 100 % 2 + 1
 console.log(angka4)
+const angka5 = 70 - 10 % 5
 console.log(angka5)
--- nomber-2.js
+++ nomber-2.js
@@ -1,33 +1,32 @@
 let lebihBesar1 = 10 > 10
-let lebihBesar2 = 10 > 100
-let lebihkecil1 = 20 < 18
-let lebihkecil2 = 49 < 82
-let lebihBesarSamaDengan1 = 90 >= 90
-let lebihBesarSamaDengan2 = 100 >= 29
-let lebihKecilSamaDengan1 = 27 <= 72
-let lebihKecilSamaDengan2 = 36 <= 71
-let equal1 = 10 == 10
-let equal2 = 90 == 10
-let notEqual1 = 90 != 73
-let notEqual2 = 28 != 37
-let equalTo1 = 10 === '10'
-let equalTo2 = 90 === '90'
-let notEqualTo1 = 72 !== '82'
-let notEqualTo2 = true !== 1
-
 console.log(lebihBesar1)
+let lebihBesar2 = 10 > 100
 console.log(lebihBesar2)
+let lebihkecil1 = 20 < 18
 console.log(lebihkecil1)
+let lebihkecil2 = 49 < 82
 console.log(lebihkecil2)
+let lebihBesarSamaDengan1 = 90 >= 90
 console.log(lebihBesarSamaDengan1)
+let lebihBesarSamaDengan2 = 100 >= 29
 console.log(lebihBesarSamaDengan2)
+let lebihKecilSamaDengan1 = 27 <= 72
 console.log(lebihKecilSamaDengan1)
+let lebihKecilSamaDengan2 = 36 <= 71
 console.log(lebihKecilSamaDengan2)
+let equal1 = 10 == 10
 console.log(equal1)
+let equal2 = 90 == 10
 console.log(equal2)
-console.log(equalTo1)
-console.log(equalTo2)
+let notEqual1 = 90 != 73
 console.log(notEqual1)
+let notEqual2 = 28 != 37
 console.log(notEqual2)
+let equalTo1 = 10 === '10'
+console.log(equalTo1)
+let equalTo2 = 90 === '90'
+console.log(equalTo2)
+let notEqualTo1 = 72 !== '82'
 console.log(notEqualTo1)
+let notEqualTo2 = true !== 1
 console.log(notEqualTo2)
